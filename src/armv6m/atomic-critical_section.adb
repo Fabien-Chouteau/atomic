6e66298e11f31c000ee7f2a53e2608ff42209d55
Fabien Chouteau <chouteau@adacore.com>
2021-09-13 12:00:46 +0200

    Add support of CPU without atomic instructions using critical sections

---
+++ src/armv6m/atomic-critical_section.adb
@@ -0,0 +1,40 @@
+with System.Machine_Code; use System.Machine_Code;
+
+package body Atomic.Critical_Section is
+
+   -----------
+   -- Enter --
+   -----------
+
+   procedure Enter (State : out Interrupt_State) is
+      NL : constant String := ASCII.CR & ASCII.LF;
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "dsb" & NL &             -- Ensure completion of memory accesses
+           "mrs %0, PRIMASK" & NL & -- Save PRIMASK State
+           "cpsid i",               -- Clear PRIMASK
+         Outputs  => Interrupt_State'Asm_Output ("=r", State),
+         Inputs   => No_Input_Operands,
+         Clobber  => "",
+         Volatile => True);
+   end Enter;
+
+   -----------
+   -- Leave --
+   -----------
+
+   procedure Leave (State : Interrupt_State) is
+      NL : constant String := ASCII.CR & ASCII.LF;
+   begin
+      System.Machine_Code.Asm
+        (Template =>
+           "dsb" & NL &       -- Ensure completion of memory accesses
+           "mrs %0, PRIMASK", -- restore PRIMASK
+         Outputs  => No_Output_Operands,
+         Inputs   => Interrupt_State'Asm_Input ("r", State),
+         Clobber  => "r1",
+         Volatile => True);
+   end Leave;
+
+end Atomic.Critical_Section;
---
+++ src/critical_section/atomic-signed.adb
@@ -0,0 +1,278 @@
+with Atomic.Critical_Section; use Atomic.Critical_Section;
+
+with System;
+
+package body Atomic.Signed
+with SPARK_Mode => Off
+is
+
+   ----------
+   -- Load --
+   ----------
+
+   function Load
+     (This : aliased Instance;
+      Order : Mem_Order := Seq_Cst)
+      return T
+   is
+      State : Interrupt_State;
+      Result : T;
+   begin
+      Critical_Section.Enter (State);
+      Result := T (This);
+      Critical_Section.Leave (State);
+      return Result;
+   end Load;
+
+   -----------
+   -- Store --
+   -----------
+
+   procedure Store
+     (This  : aliased in out Instance;
+      Val   : T;
+      Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      This := Instance (Val);
+      Critical_Section.Leave (State);
+   end Store;
+
+   ---------
+   -- Add --
+   ---------
+
+   procedure Add (This  : aliased in out Instance;
+                  Val   : T;
+                  Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      This := This + Instance (Val);
+      Critical_Section.Leave (State);
+   end Add;
+
+   ---------
+   -- Sub --
+   ---------
+
+   procedure Sub (This  : aliased in out Instance;
+                  Val   : T;
+                  Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      This := This - Instance (Val);
+      Critical_Section.Leave (State);
+   end Sub;
+
+   -- SPARK compatible --
+
+   --------------
+   -- Exchange --
+   --------------
+
+   procedure Exchange (This  : aliased in out Instance;
+                       Val   : T;
+                       Old   : out T;
+                       Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      Old := T (This);
+      This := Instance (Val);
+      Critical_Section.Leave (State);
+   end Exchange;
+
+   ----------------------
+   -- Compare_Exchange --
+   ----------------------
+
+   procedure Compare_Exchange (This          : aliased in out Instance;
+                               Expected      : T;
+                               Desired       : T;
+                               Weak          : Boolean;
+                               Success       : out Boolean;
+                               Success_Order : Mem_Order := Seq_Cst;
+                               Failure_Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      if T (This) = Expected then
+         This := Instance (Desired);
+         Success := True;
+      else
+         Success := False;
+      end if;
+      Critical_Section.Leave (State);
+   end Compare_Exchange;
+
+   ---------------
+   -- Add_Fetch --
+   ---------------
+
+   procedure Add_Fetch (This  : aliased in out Instance;
+                        Val   : T;
+                        Result : out T;
+                        Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      This := This + Instance (Val);
+      Result := T (This);
+      Critical_Section.Leave (State);
+   end Add_Fetch;
+
+   ---------------
+   -- Sub_Fetch --
+   ---------------
+
+   procedure Sub_Fetch (This  : aliased in out Instance;
+                       Val   : T;
+                        Result : out T;
+                        Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      This := This - Instance (Val);
+      Result := T (This);
+      Critical_Section.Leave (State);
+   end Sub_Fetch;
+
+   ---------------
+   -- Fetch_Add --
+   ---------------
+
+   procedure Fetch_Add (This  : aliased in out Instance;
+                        Val   : T;
+                        Result : out T;
+                        Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      Result := T (This);
+      This := This + Instance (Val);
+      Critical_Section.Leave (State);
+   end Fetch_Add;
+
+   ---------------
+   -- Fetch_Sub --
+   ---------------
+
+   procedure Fetch_Sub (This  : aliased in out Instance;
+                       Val   : T;
+                        Result : out T;
+                        Order : Mem_Order := Seq_Cst)
+   is
+      State : Interrupt_State;
+   begin
+      Critical_Section.Enter (State);
+      Result := T (This);
+      This := This - Instance (Val);
+      Critical_Section.Leave (State);
+   end Fetch_Sub;
+
+   -- NOT SPARK Compatible --
+
+   --------------
+   -- Exchange --
+   --------------
+
+   function Exchange
+     (This  : aliased in out Instance;
+      Val   : T;
+      Order : Mem_Order := Seq_Cst)
+      return T
+   is
+      Result : T;
+   begin
+      Exchange (This, Val, Result, Order);
+      return Result;
+   end Exchange;
+
+   ----------------------
+   -- Compare_Exchange --
+   ----------------------
+
+   function Compare_Exchange
+     (This          : aliased in out Instance;
+      Expected      : T;
+      Desired       : T;
+      Weak          : Boolean;
+      Success_Order : Mem_Order := Seq_Cst;
+      Failure_Order : Mem_Order := Seq_Cst)
+      return Boolean
+   is
+      Result : Boolean;
+   begin
+      Compare_Exchange (This, Expected, Desired, Weak, Result, Success_Order,
+                        Failure_Order);
+      return Result;
+   end Compare_Exchange;
+
+   ---------------
+   -- Add_Fetch --
+   ---------------
+
+   function Add_Fetch
+     (This : aliased in out Instance; Val : T; Order : Mem_Order := Seq_Cst)
+      return T
+   is
+      Result : T;
+   begin
+      Add_Fetch (This, Val, Result, Order);
+      return Result;
+   end Add_Fetch;
+
+   ---------------
+   -- Sub_Fetch --
+   ---------------
+
+   function Sub_Fetch
+     (This : aliased in out Instance; Val : T; Order : Mem_Order := Seq_Cst)
+      return T
+   is
+      Result : T;
+   begin
+      Sub_Fetch (This, Val, Result, Order);
+      return Result;
+   end Sub_Fetch;
+
+   ---------------
+   -- Fetch_Add --
+   ---------------
+
+   function Fetch_Add
+     (This : aliased in out Instance; Val : T; Order : Mem_Order := Seq_Cst)
+      return T
+   is
+      Result : T;
+   begin
+      Fetch_Add (This, Val, Result, Order);
+      return Result;
+   end Fetch_Add;
+
+   ---------------
+   -- Fetch_Sub --
+   ---------------
+
+   function Fetch_Sub
+     (This : aliased in out Instance; Val : T; Order : Mem_Order := Seq_Cst)
+      return T
+   is
+      Result : T;
+   begin
+      Fetch_Sub (This, Val, Result, Order);
+      return Result;
+   end Fetch_Sub;
+
+end Atomic.Signed;
